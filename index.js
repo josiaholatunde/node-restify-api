4eca1fea3f2f768ff7145f85aaa338075296bc30
Ogunboyejo Olatunde <tunde.ogunboyejo@xputertechnologies.com>
2019-05-31 08:53:34 +0100

    added profile

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const restify = require('restify');
 const mongoose = require('mongoose');
 const config = require('./config');
 const rjwt = require('restify-jwt-community');
+const morgan = require('morgan');
 
 const server = restify.createServer();
 
@@ -9,6 +10,7 @@ server.use(restify.plugins.bodyParser());
 
 server.use(rjwt({secret: config.JWT_SECRET}).unless({path: ['/auth/login','/auth/register']}));
 
+server.use(morgan('dev'));
 server.listen(config.PORT, () => {
     mongoose.set('useFindAndModify', false);
     mongoose.connect(config.MONGODB_URI, {useNewUrlParser: true});
